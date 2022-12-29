8809eefd79908e153d0a407073b09be710bbf2ff
ajh123 <ajh123@users.noreply.github.com>
2022-12-29 16:22:59 +0000

    Fix files

--- common/src/main/java/tk/minersonline/CreateLogicals/CreateLogicals.java
+++ common/src/main/java/tk/minersonline/CreateLogicals/CreateLogicals.java
@@ -2,7 +2,7 @@ package tk.minersonline.CreateLogicals;
 
 public class CreateLogicals
 {
-	public static final String MOD_ID = "CreateLogicals";
+	public static final String MOD_ID = "create_logics";
 
 	public static void init() {
 		
